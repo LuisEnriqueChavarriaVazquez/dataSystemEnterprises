50792710ffb8c31ac00a59405c45d8f3a59f2947
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-07-30 11:08:46 -0600

    Primera version del buscador de empresas

--- js/buscadores/buscadorEmpresas.js
+++ js/buscadores/buscadorEmpresas.js
@@ -1,24 +1,26 @@
 // URL del archivo HTML en GitHub (crudo/raw)
-var url = 'https://raw.githubusercontent.com/LuisEnriqueChavarriaVazquez/getInvestingServiceData/main/generatedFiles/intel-corp-balance-sheet.html';
-var url_two = 'https://raw.githubusercontent.com/LuisEnriqueChavarriaVazquez/getInvestingServiceData/main/generatedFiles/intel-corp-income-statement.html';
+var url =
+  "https://raw.githubusercontent.com/LuisEnriqueChavarriaVazquez/getInvestingServiceData/main/generatedFiles/intel-corp-balance-sheet.html";
+var url_two =
+  "https://raw.githubusercontent.com/LuisEnriqueChavarriaVazquez/getInvestingServiceData/main/generatedFiles/intel-corp-income-statement.html";
 
 // Realizar la solicitud GET al archivo HTML
 fetch(url)
-    .then(response => response.text())
-    .then(html => {
-        // Acceder al contenido del archivo HTML
-        document.getElementById('balance_sheet').innerHTML = html;
-    })
-    .catch(error => {
-        console.error('Recurso no disponible:', error);
-    });
+  .then((response) => response.text())
+  .then((html) => {
+    // Acceder al contenido del archivo HTML
+    document.getElementById("balance_sheet").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Recurso no disponible:", error);
+  });
 
 fetch(url_two)
-    .then(response => response.text())
-    .then(html => {
-        // Acceder al contenido del archivo HTML
-        document.getElementById('income_statement').innerHTML = html;
-    })
-    .catch(error => {
-        console.error('Recurso no disponible:', error);
-    });
+  .then((response) => response.text())
+  .then((html) => {
+    // Acceder al contenido del archivo HTML
+    document.getElementById("income_statement").innerHTML = html;
+  })
+  .catch((error) => {
+    console.error("Recurso no disponible:", error);
+  });
