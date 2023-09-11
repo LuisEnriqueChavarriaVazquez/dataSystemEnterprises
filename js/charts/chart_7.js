132066a710338e4b1858d6f3158b3da785677385
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-09-11 06:26:19 -0600

    índice de rentabilidad graficado :)

--- js/charts/chart_7.js
+++ js/charts/chart_7.js
@@ -1,12 +1,25 @@
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+//Graficas
+
+//Accedo a los datos en memoria
+    // //Si quieres imprimir nombres hazlo asi
+    // let exampleNombres = localStorage.getItem('nombreEmpresaGraph').split(",");
+    // console.log(exampleNombres);
+
+    // //Si quieres imprimir numeros hazlo asi...
+    // console.log(JSON.parse(localStorage.getItem('endeudamientoEmpresaGraph')));
+
 const chart_7 = document.getElementById("chart_7");
 
-const labels = ["Empresa 1", "Empresa 2", "Empresa 3", "Empresa 4", "Empresa 5", "Empresa 6", "Empresa 7"];
+const labels = localStorage.getItem('nombreEmpresaGraph').split(",");
 const data_chart_7 = {
   labels: labels,
   datasets: [{
     axis: 'y',
-    label: 'Datos del índice',
-    data: [65, 59, 80, 81, 56, 55, 40],
+    label: 'índice de rentabilidad',
+    data: JSON.parse(localStorage.getItem('rentabilidadEmpresaGraph')),
     fill: true,
     backgroundColor: backgroundColor_dinamico,
     borderColor: borderColor_dinamico,
---
+++ js/charts/obtenerDataParaGraficos.js
@@ -0,0 +1,52 @@
+/**
+ * Lo que esta funcion hace es guardar los datos en memoria que fueron obteneidos despues de la busqueda, 
+ * pero los obtiene del JSON que estaba en local storage
+ * 
+ * Con esto se consigue guardarlos en arrays por separado.
+ * 
+ * Con lo que al final d¿se salvan recursos, teniendo cada dato en arrays mas ligeros en lugar de un obj json grande
+ */
+
+function guardarTodaLaDataEnArraysMemoria(){   
+
+    //Accedemos a los datos de las empresas
+    let datosEmpresaAlmacenados = localStorage.getItem('empresaConIndices');
+    let datosJsonParaGrafica = JSON.parse(datosEmpresaAlmacenados);
+
+    //Reservamos el espacio para los arrays de nombres de empresas
+    let nombreEmpresaGraph = [];
+    let rentabilidadEmpresaGraph = [];
+    let endeudamientoEmpresaGraph = [];
+    let rotacionEmpresaGraph = [];
+    let liquidezEmpresaGraph = [];
+    
+    //Guardamos los datos provenientes del JSON que estaba en memoria
+    for (let empresa in datosJsonParaGrafica) {
+        if (datosJsonParaGrafica.hasOwnProperty(empresa)) {
+            nombreEmpresaGraph.push(empresa);
+            rentabilidadEmpresaGraph.push(datosJsonParaGrafica[empresa].rentabilidad);
+            endeudamientoEmpresaGraph.push(datosJsonParaGrafica[empresa].endeudamiento);
+            rotacionEmpresaGraph.push(datosJsonParaGrafica[empresa].rotacion);
+            liquidezEmpresaGraph.push(datosJsonParaGrafica[empresa].liquidez);
+        }
+    }
+
+    localStorage.setItem("nombreEmpresaGraph", nombreEmpresaGraph);
+    localStorage.setItem("rentabilidadEmpresaGraph", "[" + rentabilidadEmpresaGraph + "]");
+    localStorage.setItem("endeudamientoEmpresaGraph", "[" + endeudamientoEmpresaGraph + "]");
+    localStorage.setItem("rotacionEmpresaGraph", "[" + rotacionEmpresaGraph + "]");
+    localStorage.setItem("liquidezEmpresaGraph", "[" + liquidezEmpresaGraph + "]");
+
+
+    //Si quieres imprimir nombres hazlo asi
+    let exampleNombres = localStorage.getItem('nombreEmpresaGraph').split(",");
+    console.log(exampleNombres);
+
+    //Si quieres imprimir numeros hazlo asi...
+    console.log(JSON.parse(localStorage.getItem('endeudamientoEmpresaGraph')));
+}
+
+
+
+//Llamamos a la funcion para que guarde todo en memoria
+guardarTodaLaDataEnArraysMemoria();
