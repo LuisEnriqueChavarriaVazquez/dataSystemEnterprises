3d9a90d6e08105e36bd11dfd8b2c6f170728384d
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-09-25 14:51:22 -0600

    Indices especificos en memoria

--- js/buscadores/datosFinancierosEmpresas.js
+++ js/buscadores/datosFinancierosEmpresas.js
@@ -13,19 +13,51 @@ window.addEventListener("storage", function (event) {
       const divDatos = document.createElement("div");
       const divDatosContainer = document.createElement("section");
 
-      //Iteraciones para los indices más generales
-      let contadorIteraciones = 0;
+    //Hacemos iteraciones para los datos de indices generales
+    let contadorIteraciones = 0;
 
-      for (let dato in empresas[nombreEmpresa]) {
-        if(contadorIteraciones <= 3){
-          const p = document.createElement("p");
-          p.textContent = `${dato}: ${empresas[nombreEmpresa][dato]}`;
-          divDatos.appendChild(p);
-          contadorIteraciones++;
-        }else{
-          contadorIteraciones = 0;
-        }
+    for (let dato in empresas[nombreEmpresa]) {
+      if(contadorIteraciones <= 3){
+        const p = document.createElement("p");
+        p.innerHTML = `<b><i>Índice general de ${dato}: </i></b> ${empresas[nombreEmpresa][dato]}`;
+        divDatos.appendChild(p);
+        contadorIteraciones++;
+      }else{
+        contadorIteraciones = 0;
+        break;
       }
+    }
+
+    // Acceder a los índices específicos y mostrarlos
+    const indicesEspecificosRentabilidad = empresas[nombreEmpresa].indices_rentabilidad_especificos;
+    const indicesEspecificosLiquidez = empresas[nombreEmpresa].indices_liquidez_especificos;
+    const indicesEspecificosEndeudamiento = empresas[nombreEmpresa].indices_endeudamiento_especificos;
+    const indicesEspecificosRotacion = empresas[nombreEmpresa].indices_rotacion_especificos;
+
+    for (let indice in indicesEspecificosRentabilidad) {
+      const p = document.createElement("p");
+      p.innerHTML = `<b><i>Rentabilidad ${indice}: </i></b> ${indicesEspecificosRentabilidad[indice] || 0}`;
+      divDatos.appendChild(p);
+    }
+
+    for (let indice in indicesEspecificosLiquidez) {
+      const p = document.createElement("p");
+      p.innerHTML = `<b><i>Liquidez ${indice}: </i></b> ${indicesEspecificosLiquidez[indice] || 0}`;
+      divDatos.appendChild(p);
+    }
+
+    for (let indice in indicesEspecificosEndeudamiento) {
+      const p = document.createElement("p");
+      p.innerHTML = `<b><i>Endeudamiento ${indice}: </i></b> ${indicesEspecificosEndeudamiento[indice] || 0}`;
+      divDatos.appendChild(p);
+    }
+
+    for (let indice in indicesEspecificosRotacion) {
+      const p = document.createElement("p");
+      p.innerHTML = `<b><i>Rotación ${indice}: </i></b> ${indicesEspecificosRotacion[indice] || 0}`;
+      divDatos.appendChild(p);
+    }
+
 
       divDatosContainer.appendChild(h1);
       divDatosContainer.appendChild(divDatos);
--- js/charts/obtenerDataParaGraficos.js
+++ js/charts/obtenerDataParaGraficos.js
@@ -20,24 +20,131 @@ function guardarTodaLaDataEnArraysMemoria(){
     let endeudamientoEmpresaGraph = [];
     let rotacionEmpresaGraph = [];
     let liquidezEmpresaGraph = [];
+
+    let rentabilidad_especifica = [];
+    let endeudamiento_especifica = [];
+    let rotacion_especifica = [];
+    let liquidez_especifica = [];
     
     //Guardamos los datos provenientes del JSON que estaba en memoria
     for (let empresa in datosJsonParaGrafica) {
         if (datosJsonParaGrafica.hasOwnProperty(empresa)) {
+            //Indices generales
             nombreEmpresaGraph.push(empresa);
             rentabilidadEmpresaGraph.push((datosJsonParaGrafica[empresa].rentabilidad));
             endeudamientoEmpresaGraph.push(datosJsonParaGrafica[empresa].endeudamiento);
             rotacionEmpresaGraph.push(datosJsonParaGrafica[empresa].rotacion);
             liquidezEmpresaGraph.push(datosJsonParaGrafica[empresa].liquidez);
+
+            //Indices particulares... contiene un array con varios objetos dentro
+            rentabilidad_especifica.push(datosJsonParaGrafica[empresa].indices_rentabilidad_especificos);
+            endeudamiento_especifica.push(datosJsonParaGrafica[empresa].indices_endeudamiento_especificos);
+            rotacion_especifica.push(datosJsonParaGrafica[empresa].indices_rotacion_especificos);
+            liquidez_especifica.push(datosJsonParaGrafica[empresa].indices_liquidez_especificos);
         }
     }
 
+    //Separamos los indices especificos en array particulares
+    //(1) Indices de rentabilidad
+    function separarIndicesRentabilidad(datos) {
+        const margenDeUtilidad = [];
+        const rendimientosSobreActivosTotales = [];
+        const rendimientosSobreCapitalContable = [];
+      
+        for (let i = 0; i < datos.length; i++) {
+          margenDeUtilidad.push(Number(datos[i].margen_de_utilidad));
+          rendimientosSobreActivosTotales.push(Number(datos[i].rendimientos_sobre_activos_totales));
+          rendimientosSobreCapitalContable.push(Number(datos[i].rendimientos_sobre_capital_contable));
+        }
+      
+        return {
+          margenDeUtilidad,
+          rendimientosSobreActivosTotales,
+          rendimientosSobreCapitalContable,
+        };
+    }
+      
+    let indicesSeparadosRentabilidad = separarIndicesRentabilidad(rentabilidad_especifica);
+    localStorage.setItem("indicesSeparadosRentabilidad", indicesSeparadosRentabilidad);
+
+    //(2) Indices de liquidez
+    function separarIndicesLiquidez(datos) {
+        const capital_de_trabajo = [];
+        const capital_operativo = [];
+        const prueba_del_acido = [];
+        const razon_de_liquidez = [];
+      
+        for (let i = 0; i < datos.length; i++) {
+          capital_de_trabajo.push(Number(datos[i].capital_de_trabajo));
+          capital_operativo.push(Number(datos[i].capital_operativo));
+          prueba_del_acido.push(Number(datos[i].prueba_del_acido));
+          razon_de_liquidez.push(Number(datos[i].razon_de_liquidez));
+        }
+      
+        return {
+          capital_de_trabajo,
+          capital_operativo,
+          prueba_del_acido,
+          razon_de_liquidez,
+        };
+    }
+      
+    let indicesSeparadosLiquidez = separarIndicesLiquidez(liquidez_especifica);
+    localStorage.setItem("indicesSeparadosLiquidez", indicesSeparadosLiquidez);
+
+    //(3) Indices de endeudamiento
+    function separarIndicesEndeudamiento(datos) {
+        const razon_de_cobertura_de_interes = [];
+        const apalancamiento_sobre_activo_total = [];
+        
+        for (let i = 0; i < datos.length; i++) {
+            razon_de_cobertura_de_interes.push(Number(datos[i].razon_de_cobertura_de_interes));
+            apalancamiento_sobre_activo_total.push(Number(datos[i].apalancamiento_sobre_activo_total));
+        }
+        
+        return {
+            razon_de_cobertura_de_interes,
+            apalancamiento_sobre_activo_total,
+        };
+    }
+        
+    let indicesSeparadosEndeudamiento = separarIndicesEndeudamiento(endeudamiento_especifica);
+    localStorage.setItem("indicesSeparadosEndeudamiento", indicesSeparadosEndeudamiento);
+
+    //(4) Indices de rotacion
+    function separarIndicesRotacion(datos) {
+        const rotacion_de_activos_fijos = [];
+        const rotacion_de_activos_totales = [];
+        const rotacion_de_cuentas_por_cobrar = [];
+        const rotacion_de_inventarios = [];
+      
+        for (let i = 0; i < datos.length; i++) {
+          rotacion_de_activos_fijos.push(Number(datos[i].rotacion_de_activos_fijos));
+          rotacion_de_activos_totales.push(Number(datos[i].rotacion_de_activos_totales));
+          rotacion_de_cuentas_por_cobrar.push(Number(datos[i].rotacion_de_cuentas_por_cobrar));
+          rotacion_de_inventarios.push(Number(datos[i].rotacion_de_inventarios));
+        }
+      
+        return {
+          rotacion_de_activos_fijos,
+          rotacion_de_activos_totales,
+          rotacion_de_cuentas_por_cobrar,
+          rotacion_de_inventarios,
+        };
+    }
+      
+    let indicesSeparadosRotacion = separarIndicesRotacion(rotacion_especifica);
+    localStorage.setItem("indicesSeparadosRotacion", indicesSeparadosRotacion);
+    
+    //Guardamos indices generales en memoria
     localStorage.setItem("nombreEmpresaGraph", nombreEmpresaGraph);
     localStorage.setItem("rentabilidadEmpresaGraph", "[" + rentabilidadEmpresaGraph + "]");
     localStorage.setItem("endeudamientoEmpresaGraph", "[" + endeudamientoEmpresaGraph + "]");
     localStorage.setItem("rotacionEmpresaGraph", "[" + rotacionEmpresaGraph + "]");
     localStorage.setItem("liquidezEmpresaGraph", "[" + liquidezEmpresaGraph + "]");
 
+    //Si quieres imprimir los datos de indices especificos hazle asi
+    console.log("Margen de Utilidad:", indicesSeparadosRentabilidad.margenDeUtilidad);
 
     //Si quieres imprimir nombres hazlo asi
     let exampleNombres = localStorage.getItem('nombreEmpresaGraph').split(",");
