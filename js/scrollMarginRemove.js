0dd09cfa7351d4bba6b7dc24f2b7eb243ec0cd15
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-10-15 21:55:50 -0600

    Menu del buscador listo

--- js/scrollMarginRemove.js
+++ js/scrollMarginRemove.js
@@ -8,20 +8,41 @@ let mainContainerElementInitialState = document.getElementById('mainContainerEle
 let menuContenidoPrincipal = document.getElementById('menuContenidoPrincipal');
 let contenidoPrincipal = document.getElementById('contenidoPrincipal');
 
+//Contenedor del input del buscador
+let inputBrowser_empresasScroll = document.getElementById('inputBrowser_empresas');
+
 contenidoPrincipal.addEventListener('scroll', function() {
     var scrollPosition = contenidoPrincipal.scrollTop; //Position actual de scroll en div
     var scrollHeight = contenidoPrincipal.scrollHeight; //altura total de la caja de contenido con scroll
     var clientHeight = contenidoPrincipal.clientHeight; //altura total de la caja sin scroll (solo visible)
   
-    //Cuando hallamos recorrido una distancia igual a la altura visible
-    if (scrollPosition > clientHeight) {
-        mainContainerElementInitialState.setAttribute('style', 'padding: 0px;');
-        menuContenidoPrincipal.setAttribute('style', 'border-radius: 0px;');
-        contenidoPrincipal.setAttribute('style', 'border-radius: 0px;');
-    }else if(scrollPosition <= 200){
-        mainContainerElementInitialState.setAttribute('style', 'padding: 30px;');
-        menuContenidoPrincipal.setAttribute('style', 'border-radius: 15px 0 0 15px;');
-        contenidoPrincipal.setAttribute('style', 'border-radius: 0 15px 15px 0;');
+    // //Cuando hallamos recorrido una distancia igual a la altura visible
+    // if (scrollPosition > clientHeight) {
+    //     mainContainerElementInitialState.setAttribute('style', 'padding: 0px;');
+    //     menuContenidoPrincipal.setAttribute('style', 'border-radius: 0px;');
+    //     contenidoPrincipal.setAttribute('style', 'border-radius: 0px;');
+
+    //     setTimeout(() => {
+    //         inputBrowser_empresasScroll.setAttribute('style', "width:" + (window.innerWidth - menuContenidoPrincipal.offsetWidth) + "px; left:" + (menuContenidoPrincipal.offsetWidth) + "px; top:" + "0px; border-radius: 0px;");
+    //     }, 100);
+
+    // }else if(scrollPosition <= 200){
+    //     mainContainerElementInitialState.setAttribute('style', 'padding: 30px;');
+    //     menuContenidoPrincipal.setAttribute('style', 'border-radius: 15px 0 0 15px;');
+    //     contenidoPrincipal.setAttribute('style', 'border-radius: 0 15px 15px 0;');
+
+    //     inputBrowser_empresasScroll.setAttribute('style', "width:" + (window.innerWidth - menuContenidoPrincipal.offsetWidth - 60) + "px; left:" + (menuContenidoPrincipal.offsetWidth + 30) + "px; top: 30px;");
+    // }
+
+    //Damos nuevos estilos al contenedor del buscador
+    if(window.innerWidth > 1000){
+        if(scrollPosition > 50){
+            inputBrowser_empresasScroll.classList.add('inputBrowser_empresasSticked');
+            inputBrowser_empresasScroll.setAttribute('style', "width:" + (window.innerWidth - menuContenidoPrincipal.offsetWidth - 60) + "px; left:" + (menuContenidoPrincipal.offsetWidth + 30) + "px; top: 30px;");
+        }else{
+            inputBrowser_empresasScroll.classList.remove('inputBrowser_empresasSticked');
+            inputBrowser_empresasScroll.removeAttribute('style');
+        }
     }
 });
   
